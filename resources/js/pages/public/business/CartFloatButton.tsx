2a9ac2966fe24ab1ad91fb300c900d07baad0a07
Juan Manuel GR <jmgr198810@gmail.com>
2025-12-17 22:23:56 +0000

    Se cambia el float button

--- resources/js/pages/public/business/CartFloatButton.tsx
+++ resources/js/pages/public/business/CartFloatButton.tsx
@@ -11,9 +11,9 @@ export const CartFloatButton = ({ totalItems, onClick }: Props) => {
     return (
         <button
             onClick={onClick}
-            className="fixed right-4 bottom-24 flex h-14 w-14 items-center justify-center rounded-full bg-green-600 text-white shadow-xl transition-all duration-200 hover:bg-green-700 sm:bottom-6 sm:h-16 sm:w-16"
+            className="fixed right-4 bottom-20 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-green-600 text-white shadow-xl transition-all duration-200 hover:bg-green-700 sm:right-20 sm:bottom-15 sm:h-10 sm:w-10 md:right-10"
         >
-            <ShoppingCart className="h-6 w-6 sm:h-7 sm:w-7" />
+            <ShoppingCart className="h-5 w-5 sm:h-5 sm:w-5" />
             {totalItems > 0 && (
                 <span className="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-xs font-bold text-white sm:h-6 sm:w-6 sm:text-sm">
                     {totalItems}
