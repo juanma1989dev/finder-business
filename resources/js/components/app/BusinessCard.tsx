d565b54a2553e8fb9d153eb2f0c377495073d527
Juan Manuel GR <jmgr198810@gmail.com>
2025-12-17 22:03:17 +0000

    Se cambia la UI de la car de los negocios

--- resources/js/components/app/BusinessCard.tsx
+++ resources/js/components/app/BusinessCard.tsx
@@ -1,12 +1,5 @@
 import { Link } from '@inertiajs/react';
 import { MapPinned } from 'lucide-react';
-// import {
-//     Select,
-//     SelectContent,
-//     SelectItem,
-//     SelectTrigger,
-//     SelectValue,
-// } from '../ui/select';
 
 interface Props {
     business: any;
@@ -15,85 +8,53 @@ interface Props {
 
 export default function BusinessCard({ business, modeEdit }: Props) {
     return (
-        <div className="group relative overflow-hidden rounded-2xl border border-gray-100 bg-red-800 bg-white shadow-sm transition-all duration-300">
-            <div className="absolute top-2 right-2 z-10">
-                {/* <Select onValueChange={(value) => console.log(value)}>
-                    <SelectTrigger className="text-gray/90 w-full rounded-lg border-none bg-white/80 px-3 py-1.5 transition duration-200 hover:bg-white/90">
-                        <SelectValue
-                            placeholder="Sin lista asignada"
-                            className="text-white/90"
-                        />
-                    </SelectTrigger>
-
-                    <SelectContent className="rounded-lg border border-white/10 bg-gray-800 text-white shadow-lg">
-                        <SelectItem
-                            value="opcion1"
-                            className="rounded-md px-2 py-1 transition-colors duration-150 hover:bg-white/10"
-                        >
-                            Opción 1
-                        </SelectItem>
-                        <SelectItem
-                            value="opcion2"
-                            className="rounded-md px-2 py-1 transition-colors duration-150 hover:bg-white/10"
-                        >
-                            Opción 2
-                        </SelectItem>
-                        <SelectItem
-                            value="opcion3"
-                            className="rounded-md px-2 py-1 transition-colors duration-150 hover:bg-white/10"
-                        >
-                            Opción 3
-                        </SelectItem>
-                    </SelectContent>
-                </Select> */}
-            </div>
-
-            <div className="relative h-48 overflow-hidden">
-                <Link href={`/business/detail/${business.id}`} className="">
+        <Link href={`/business/detail/${business.id}`} className="">
+            <div className="group cursor-pointer overflow-hidden rounded-2xl border border-gray-100 bg-white transition-all duration-300 hover:shadow-xl">
+                <div className="relative h-44 overflow-hidden">
                     <img
                         src={
                             business.cover_image
                                 ? `/storage/${business.cover_image}`
                                 : `/images/${business.category.image}`
                         }
-                        alt={business?.category.name}
-                        className="h-full w-full object-cover transition-transform duration-300"
+                        alt={business.nombre}
+                        className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-105"
                     />
-                </Link>
-                {/* <div className="absolute top-3 right-3 flex items-center space-x-1 rounded-full bg-white/90 px-2 py-1 shadow-sm backdrop-blur-sm">
-                    <Star className="h-3 w-3 fill-current text-yellow-500" /> 5
-                </div> */}
-                <div className="absolute bottom-3 left-3">
-                    <span className="inline-block rounded-full bg-orange-600 px-3 py-1 text-xs text-white shadow-sm">
-                        {business?.category.name}
-                    </span>
-                </div>
-            </div>
 
-            {/* Contenido */}
-            <div className="p-5">
-                <div className="m-0 w-full p-0 text-sm font-medium text-gray-500">
-                    A {business.distance} km de distancia
+                    {/* <div className="absolute right-3 bottom-3 rounded-full bg-white/90 p-2 shadow-md backdrop-blur-sm">
+                            <ShoppingCart className="h-4 w-4 text-orange-500" />
+                        </div> */}
                 </div>
-                <Link href={`/business/detail/${business.id}`} className="">
-                    <h3 className="items-cente mb-2 flex inline-block text-lg font-bold text-orange-600 hover:underline">
+                <div className="space-y-2 p-4">
+                    <h3 className="truncate font-bold text-gray-900">
+                        {business.nombre}
+                    </h3>
+                    <div className="flex items-center gap-1">
+                        <span className="text-sm text-gray-500">
+                            A {business.distance} km de distancia
+                        </span>
+                        {/* <Star className="h-4 w-4 fill-current text-orange-400" />
+                            <Star className="h-4 w-4 fill-current text-orange-400" />
+                            <Star className="h-4 w-4 fill-current text-orange-400" />
+                            <Star className="h-4 w-4 fill-current text-orange-400" />
+                            <Star className="h-4 w-4 fill-current text-orange-400" /> */}
+                    </div>
+                    <h3 className="items-cente mb-2 flex inline-block text-base font-bold text-orange-600">
                         {business.name}
                     </h3>
-                </Link>
-
-                <p className="mb-4 text-sm leading-relaxed text-gray-600">
-                    {business.description}
-                </p>
-
-                <div className="mb-4 space-y-2">
-                    <div className="text-md flex items-center space-x-2">
-                        <MapPinned className="mt-0.5 h-4 w-4 text-orange-600" />
-                        <span className="text-gray-500">
-                            {business.address}
-                        </span>
+                    <div className="flex items-center justify-between border-t pt-2">
+                        <div className="flex items-center gap-1 text-xs text-gray-500">
+                            <MapPinned className="mr-2 h-3 w-3 text-orange-600" />
+                            <span className="text-gray-500">
+                                {business.address}
+                            </span>
+                        </div>
+                        {/* <span className="rounded-md border border-green-100 bg-green-50 px-2 py-1 text-xs font-bold text-green-600">
+                                Envío
+                            </span> */}
                     </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 }
