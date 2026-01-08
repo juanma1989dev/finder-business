bf028c0d2481bc48220359cf9db242dfba8fb440
Juan Manuel GR <jmgr198810@gmail.com>
2026-01-08 19:57:48 +0000

    Se añaden rutas para los negocios

--- routes/auth.php
+++ routes/auth.php
@@ -1,53 +1,30 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\AccountsController;
+use App\Http\Controllers\Auth\Business\LoginController;
+use App\Http\Controllers\Auth\Business\RegisterController;
+use App\Http\Controllers\Auth\Client\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\Client\RegisteredUserController;
 use App\Http\Controllers\Auth\GoogleController;
-use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
+Route::get('/accounts', [AccountsController::class, 'index'])->name('accounts');
 
-    // Route::post('register', [RegisteredUserController::class, 'store'])
-    //     ->name('register.store');
-
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
-
-    Route::post('login', [AuthenticatedSessionController::class, 'store'])
-        ->name('login.store');
-
-    // Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-    //     ->name('password.request');
-
-    // Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-    //     ->name('password.email');
-
-    // Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-    //     ->name('password.reset');
-
-    // Route::post('reset-password', [NewPasswordController::class, 'store'])
-    //     ->name('password.store');
+# Rutas del cliente
+Route::middleware('guest')->prefix('client')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login.store');
 });
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('verify-email', EmailVerificationPromptController::class)
-//         ->name('verification.notice');
+#Rutas del negocio
+Route::middleware('guest')->prefix('business')->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('business.register');
+    Route::get('/register', [RegisterController::class, 'index'])->name('business.login');
+    // Route::post('/business/login', [AuthenticatedSessionController::class, 'store'])->name('login.store');
+});
 
-//     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-//         ->middleware(['signed', 'throttle:6,1'])
-//         ->name('verification.verify');
-
-//     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-//         ->middleware('throttle:6,1')
-//         ->name('verification.send');
-
-//     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-//         ->name('logout');
-// });
-
-# Rutas
+# Rutas Google
 Route::get('/auth/google/login', [GoogleController::class, 'redirectToGoogleLogin'])->name('auth.google.login');
 Route::get('/auth/google/register', [GoogleController::class, 'redirectToGoogleRegister'])->name('auth.google.register');
 Route::get('/auth/google/callback', [GoogleController::class, 'handleGoogleCallback'])->name('auth.google.callback');
@@ -57,6 +34,5 @@ Route::post('/session/privacy-accept', function () {
         'accept_privacy' => true,
         'privacy_version' => config('privacy.version'),
     ]);
-
     return redirect()->back();
 })->name('privacy.accept');
