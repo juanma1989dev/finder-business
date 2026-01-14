65449ff4c7757a0765c4032011ef36e0656354b6
Juan Manuel GR <jmgr198810@gmail.com>
2026-01-14 21:50:41 +0000

    Se añade la ruta para confirmar el carrito

--- app/Http/Controllers/Public/ShoppingCartController.php
+++ app/Http/Controllers/Public/ShoppingCartController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class ShoppingCartController extends Controller
 {
     public function index()
     {
         return inertia('public/ShoppingCart/Details');
+    }
+
+    public function create(Request $request)
+    {
+        $data = $request->all();
+
+        return redirect()->back()->with('success', $data);
     }
 }
