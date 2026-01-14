fc2d0eceba310320d8378799a0be8ebdc159a0c8
Juan Manuel GR <jmgr198810@gmail.com>
2026-01-14 21:50:32 +0000

    Se añade la ruta para confirmar el carrito

--- routes/public.php
+++ routes/public.php
@@ -8,4 +8,9 @@ Route::get('/', [BusinessController::class, 'index'])->name('public.home');
 Route::get('/favorites', [BusinessController::class, 'favorites'])->name('public.favorites');
 Route::post('/business/detail/set-favorite', [BusinessController::class, 'setFavorite']);
 Route::get('/business/detail/{id}', [BusinessController::class, 'details'])->name('public.business.detail');
-Route::get('/shopping-cart/details', [ShoppingCartController::class, 'index'])->name('shopping.cart.details');
+
+
+Route::prefix('/shopping-cart')->group(function() {
+    Route::get('/details', [ShoppingCartController::class, 'index'])->name('shopping.cart.details'); // change for show
+    Route::post('/', [ShoppingCartController::class, 'create'])->name('shopping.cart.create');
+});
