fad7a1f0769bb3b757ee786e81aec5bfd59791ff
Juan Manuel GR <jmgr198810@gmail.com>
2026-01-20 17:35:21 +0000

    Las cuentas se vuelven dinamicas

--- app/Http/Controllers/Auth/AccountsController.php
+++ app/Http/Controllers/Auth/AccountsController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\UserTypeEnum;
 use App\Http\Controllers\Controller;
 
 class AccountsController extends Controller
 {
     public function index()
     {
+        $accountTypes = config("login"); 
+
         return inertia('auth/accounts', [
+            'accountTypes' => $accountTypes
         ]);
     }
 }
