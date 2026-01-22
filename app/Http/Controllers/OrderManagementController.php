2e4495297d4aac52c6494190c56f9e30a39872c6
Juan Manuel GR <jmgr198810@gmail.com>
2026-01-22 18:26:24 +0000

    Se añaden campos para poder asignar masivamente

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -3,82 +3,54 @@
 namespace App\Http\Controllers;
 
 use App\Enums\OrderStatusEnum;
+use App\Flows\OrderFlow;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrderManagementController extends Controller
 {
     public function updateStatus( Request $request, Order $order)
     {
-        // dd(  $request->input('status')  );
+        $current    = strtolower( $order->status );
+        $nextStatus = strtolower( $request->input('status', null) ); // validate is required
 
-        $current    = $order->status;
-        $nextStatus =  $this->getNextStatus($current, $request);
-
-        if (!$nextStatus) {
-            return back()->with('error', 'No hay estado siguiente válido');
+        $canTransition = $this->canTransition($current, $nextStatus);
+        
+        if(!$canTransition) {
+            return back()->with('error', 'No se puede actualizar el estado del pedido.');
         }
 
         $order->update([
             'status' => $nextStatus,
         ]);
 
+
+        dd( $nextStatus,  OrderStatusEnum::READY_FOR_PICKUP->value );
+
+        if ($nextStatus === OrderStatusEnum::READY_FOR_PICKUP->value) {
+            $this->onReadyForPickup($order);
+        }
+
         return back()->with('success', 'Pedido actualizado');
     }
 
-    private function getNextStatus(string $current, Request $request): string
+    private function onReadyForPickup(Order $order)
     {
+        $order->update([
+            'delivery_id' => null,
+            'ready_for_pickup_at' => now(),  
+        ]);
 
-        $status = $request->input('status', null);
-        
-        if($status){
-            return OrderStatusEnum::fromValue($status);
-        }
-
-        $flow = OrderStatusEnum::flow();
-
-        // if (! isset($flow[$current]) || empty($flow[$current])) {
-        //     return back()->with('error', 'El pedido no puede avanzar');
-        // }
-
-        $nextStatus = collect($flow[$current])
-            ->first(fn ($s) => ! in_array($s, [
-                OrderStatusEnum::CANCELLED->value,
-                OrderStatusEnum::REJECTED->value,
-            ]));
-
-        return $nextStatus;
+        cache()->forget('available_orders');
     }
 
-
-    private function canTransition(string $from, string $to): bool
+    private function canTransition(string $current, string $next): bool
     {
+        $typeUser = Auth::user()->type;
 
-        // $flow = [
-        //     'pending' => ['confirmed', 'cancelled', 'rejected'],
-        //     'confirmed' => ['on_the_way', 'cancelled'],
-        //     'on_the_way' => ['delivered'],
-        // ];
+        $flow = OrderFlow::flow()[$typeUser] ?? [];
 
-        $flow = OrderStatusEnum::flow();
-
-
-        return in_array($to, $flow[$from] ?? []);
+        return in_array($next, $flow[$current] );
     }
-
-
-
-    // public function updateStatus(Request $request, Order $order)
-    // {
-    //     $request->validate([
-    //         'status' => ['required', Rule::in(array_column(OrderStatusEnum::cases(), 'value'))],
-    //     ]);
-
-    //     $order->status = $request->status;
-    //     $order->save();
-
-    //     return response()->json(['success' => true]);
-    // }
-
-
 }
