d9858695994c38502016ba0af897b0badee86485
Juan Manuel GR <jmgr198810@gmail.com>
2026-01-23 19:14:15 +0000

    Se añade el enum para el status de la orden

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -238,3 +238,14 @@ export interface LoginConfig {
     title: string;
     subTitle: string;
 }
+
+export enum OrderStatus {
+    PENDING = 'pending',
+    CONFIRMED = 'confirmed',
+    READY_FOR_PICKUP = 'ready_for_pickup',
+    PICKED_UP = 'picked_up',
+    ON_THE_WAY = 'on_the_way',
+    DELIVERED = 'delivered',
+    CANCELLED = 'cancelled',
+    REJECTED = 'rejected',
+}
