b51e4c9d7a4036ea50084fcd25a8cf7820501b3f
Juan Manuel GR <jmgr198810@gmail.com>
2026-01-26 22:52:59 +0000

    Se añade el filtro para los estatus

--- app/Http/Controllers/Dashboard/IndexController.php
+++ app/Http/Controllers/Dashboard/IndexController.php
@@ -2,23 +2,34 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Enums\OrderStatusEnum;
 use App\Http\Controllers\Controller;
 use App\Models\Businesses;
-use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Inertia\Inertia;
 
 class IndexController extends Controller
 {   
     public function index(Request $request)
     {
-        $businesses = Businesses::where('user_id', Auth::id())->first();
-        $orders = Order::all(); // Pendiente de filtrar;
+        $user = Auth::user();
+
+        $business = Businesses::where('user_id', 1)
+        ->with([
+            'orders' => function ($query) {
+                $query->whereNotIn(
+                    'status', 
+                    [ OrderStatusEnum::CANCELLED, OrderStatusEnum::REJECTED, OrderStatusEnum::DELIVERED ]
+                ); 
+            },
+            'orders.items'
+        ])
+        ->first();
+
 
         $data = [
-           'orders' => $orders,
-           'business' => $businesses,
+           'orders' => $business->orders,
+           'business' => $business,
         ];
 
         return inertia('dashboard/business/Index', $data);
