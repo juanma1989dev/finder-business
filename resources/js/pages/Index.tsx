5443eb72dda0b3ba6eeb1bc411dc15c41e8d5d3d
Juan Manuel GR <jmgr198810@gmail.com>
2026-02-04 16:23:08 -0600

    Se cambia la grid ppara desktop

--- resources/js/pages/Index.tsx
+++ resources/js/pages/Index.tsx
@@ -203,7 +203,7 @@ export default function Index({
                                 }
                             />
                         ) : (
-                            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
                                 {businesses.map((b) => (
                                     <BusinessCard key={b.id} business={b} />
                                 ))}
