e7a837730b2633455313357e67b3c639e2464b41
Juan Manuel GR <jmgr198810@gmail.com>
2026-02-05 16:10:45 -0600

    Se crea el middleware para validar que ya se configuro la cuenta

---
+++ app/Http/Middleware/EnsureAccountTypeIsSet.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureAccountTypeIsSet
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+
+        if (! $user) {
+            return $next($request);
+        }
+
+        if (! empty($user->account_type)) {
+            return $next($request);
+        }
+
+        if ($request->routeIs('account.config.*')) {
+            return $next($request);
+        }
+
+        return redirect()->route('account.config.home');
+    }
+}
