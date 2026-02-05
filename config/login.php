5215b60353d6f6368c73d1ca172551eb7bf99bad
Juan Manuel GR <jmgr198810@gmail.com>
2026-02-05 15:37:03 -0600

    Se modifica la configgguracion del login para utlizarlo en el nuevo flujo

--- config/login.php
+++ config/login.php
@@ -4,25 +4,28 @@ use App\Enums\UserTypeEnum;
 
 return [
     UserTypeEnum::CLIENT->value => [
-        'type' => UserTypeEnum::CLIENT->value,
-        'label' => 'Cliente',
-        'banner' => '/images/banner_cliente.webp',
-        'title' => 'Bienvenido',
-        'subTitle' => 'Inicia sesión para encontrar negocios cercanos'
+        'type'          => UserTypeEnum::CLIENT->value,
+        'title'         => 'Quiero comprar',
+        'label'         => 'Cuenta de cliente',
+        'image'         => '/images/banner_cliente.webp',
+        'description'   => 'Explora negocios cercanos, realiza pedidos y sigue tus entregas.',
+        'route.start'   => 'public.home'
     ],
     UserTypeEnum::BUSINESS->value => [
-        'type' =>  UserTypeEnum::BUSINESS->value,
-        'label' => 'Negocio',
-        'banner' => '/images/banner_negocio.webp',
-        'title' => 'Bienvenido',
-        'subTitle' => 'Inicia sesión para poder gestionar tu negocio'
+        'type'          =>  UserTypeEnum::BUSINESS->value,
+        'title'         => 'Quiero vender',
+        'label'         => 'Cuenta de negocio',
+        'image'         => '/images/banner_negocio.webp',
+        'description'   => 'Publica tu negocio, recibe pedidos y administra tus ventas.',
+        'route.start'   => 'dashboard.business.index'
     ],
     UserTypeEnum::DELIVERY->value => [
-        'type' => UserTypeEnum::DELIVERY->value,
-        'label' => 'Repartidor',
-        'banner' => '/images/banner_repartitdor.webp',
-        'title' => 'Bienvenido',
-        'subTitle' => 'Inicia sesión para comenzar a relizar entregas.'
+        'type'          => UserTypeEnum::DELIVERY->value,
+        'title'         => 'Quiero repartir',
+        'label'         => 'Cuenta de repartidor',
+        'image'         => '/images/banner_repartitdor.webp',
+        'description'   => 'Acepta pedidos disponibles y genera ingresos realizando entregas.',
+        'route.start'   => 'delivery.home'
     ]
 ];
  
