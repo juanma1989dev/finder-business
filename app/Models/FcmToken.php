b877ba626b56f5d58faf6346cfb0d03b868625d0
Juan Manuel GR <jmgr198810@gmail.com>
2026-02-09 15:52:44 -0600

    Modelo para los tokens de Google

---
+++ app/Models/FcmToken.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class FcmToken extends Model
+{
+    use HasFactory;
+
+    protected $table = 'fcm_tokens';
+
+    protected $fillable = [
+        'user_id',
+        'token',
+        'device',
+        'is_active'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
